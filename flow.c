songdongsheng/passt

commit c8b520c0625b440d0dcd588af085d35cf46aae2c

flow, repair: Wait for a short while for passt-repair to connect
...and time out after that. This will be needed because of an upcoming
change to passt-repair enabling it to start before passt is started,
on both source and target, by means of an inotify watch.

Once the inotify watch triggers, passt-repair will connect right away,
but we have no guarantees that the connection completes before we
start the migration process, so wait for it (for a reasonable amount
of time).

Signed-off-by: Stefano Brivio <sbrivio@redhat.com>
Reviewed-by: David Gibson <david@gibson.dropbear.id.au>

flow.c
@@ -912,6 +912,21 @@ static int flow_migrate_source_rollback(struct ctx *c, unsigned bound, int ret)
 }
 
 /**
+ * flow_migrate_need_repair() - Do we need to set repair mode for any flow?
+ *
+ * Return: true if repair mode is needed, false otherwise
+ */
+static bool flow_migrate_need_repair(void)
+{
+	union flow *flow;
+
+	foreach_established_tcp_flow(flow)
+		return true;
+
+	return false;
+}
+
+/**
  * flow_migrate_repair_all() - Turn repair mode on or off for all flows
  * @c:		Execution context
  * @enable:	Switch repair mode on if set, off otherwise
@@ -965,6 +980,9 @@ int flow_migrate_source_pre(struct ctx *c, const struct migrate_stage *stage,
 
 	(void)stage;
 	(void)fd;
+
+	if (flow_migrate_need_repair())
+		repair_wait(c);
 
 	if ((rc = flow_migrate_repair_all(c, true)))
 		return -rc;
@@ -1082,6 +1100,8 @@ int flow_migrate_target(struct ctx *c, const struct migrate_stage *stage,
 
 	if (!count)
 		return 0;
+
+	repair_wait(c);
 
 	if ((rc = flow_migrate_repair_all(c, true)))
 		return -rc;

repair.c
@@ -27,6 +27,10 @@
 
 #define SCM_MAX_FD 253 /* From Linux kernel (include/net/scm.h), not in UAPI */
 
+/* Wait for a while for TCP_REPAIR helper to connect if it's not there yet */
+#define REPAIR_ACCEPT_TIMEOUT_MS	10
+#define REPAIR_ACCEPT_TIMEOUT_US	(REPAIR_ACCEPT_TIMEOUT_MS * 1000)
+
 /* Pending file descriptors for next repair_flush() call, or command change */
 static int repair_fds[SCM_MAX_FD];
 
@@ -136,6 +140,34 @@ void repair_handler(struct ctx *c, uint32_t events)
 	(void)events;
 
 	repair_close(c);
+}
+
+/**
+ * repair_wait() - Wait (with timeout) for TCP_REPAIR helper to connect
+ * @c:		Execution context
+ */
+void repair_wait(struct ctx *c)
+{
+	struct timeval tv = { .tv_sec = 0,
+			      .tv_usec = (long)(REPAIR_ACCEPT_TIMEOUT_US) };
+	static_assert(REPAIR_ACCEPT_TIMEOUT_US < 1000 * 1000,
+		      ".tv_usec is greater than 1000 * 1000");
+
+	if (c->fd_repair >= 0 || c->fd_repair_listen == -1)
+		return;
+
+	if (setsockopt(c->fd_repair_listen, SOL_SOCKET, SO_RCVTIMEO,
+		       &tv, sizeof(tv))) {
+		err_perror("Set timeout on TCP_REPAIR listening socket");
+		return;
+	}
+
+	repair_listen_handler(c, EPOLLIN);
+
+	tv.tv_usec = 0;
+	if (setsockopt(c->fd_repair_listen, SOL_SOCKET, SO_RCVTIMEO,
+		       &tv, sizeof(tv)))
+		err_perror("Clear timeout on TCP_REPAIR listening socket");
 }
 
 /**

repair.h
@@ -10,6 +10,7 @@ void repair_sock_init(const struct ctx *c);
 void repair_listen_handler(struct ctx *c, uint32_t events);
 void repair_handler(struct ctx *c, uint32_t events);
 void repair_close(struct ctx *c);
+void repair_wait(struct ctx *c);
 int repair_flush(struct ctx *c);
 int repair_set(struct ctx *c, int s, int cmd);
 

commit 754c6d728686c5d115bd97c628d53733776dd711

flow, repair: Proper error handling for missing passt-repair helper on target
If the target doesn't have a connected passt-repair helper,
repair_wait() sets a timeout and forcibly calls accept4(), but if the
timeout expires, we don't report any error and proceed as if we had a
connected helper.

Later, in repair_flush(), sendmsg() will fail for each flow we try to
migrate with EBADF, and we'll handle that gracefully, but it makes no
sense to even try, and it also leads to noise in the logs.

Similarly to how we handle a missing repair helper in
flow_migrate_repair_all(), propagate timeout and other errors through
repair_wait(), and don't attempt to migrate flows if repair_wait()
failed.

Fixes: c8b520c0625b ("flow, repair: Wait for a short while for passt-repair to connect")
Signed-off-by: Stefano Brivio <sbrivio@redhat.com>

flow.c
@@ -1130,7 +1130,8 @@ int flow_migrate_target(struct ctx *c, const struct migrate_stage *stage,
 	if (!count)
 		return 0;
 
-	repair_wait(c);
+	if ((rc = repair_wait(c)))
+		return -rc;
 
 	if ((rc = flow_migrate_repair_all(c, true)))
 		return -rc;

repair.c
@@ -68,18 +68,21 @@ void repair_sock_init(const struct ctx *c)
  * repair_listen_handler() - Handle events on TCP_REPAIR helper listening socket
  * @c:		Execution context
  * @events:	epoll events
+ *
+ * Return: 0 on valid event with new connected socket, error code on failure
  */
-void repair_listen_handler(struct ctx *c, uint32_t events)
+int repair_listen_handler(struct ctx *c, uint32_t events)
 {
 	union epoll_ref ref = { .type = EPOLL_TYPE_REPAIR };
 	struct epoll_event ev = { 0 };
 	struct ucred ucred;
 	socklen_t len;
+	int rc;
 
 	if (events != EPOLLIN) {
 		debug("Spurious event 0x%04x on TCP_REPAIR helper socket",
 		      events);
-		return;
+		return EINVAL;
 	}
 
 	len = sizeof(ucred);
@@ -90,18 +93,19 @@ void repair_listen_handler(struct ctx *c, uint32_t events)
 				      SOCK_NONBLOCK);
 
 		if (discard == -1)
-			return;
+			return errno;
 
 		if (!getsockopt(discard, SOL_SOCKET, SO_PEERCRED, &ucred, &len))
 			info("Discarding TCP_REPAIR helper, PID %i", ucred.pid);
 
 		close(discard);
-		return;
+		return EEXIST;
 	}
 
 	if ((c->fd_repair = accept4(c->fd_repair_listen, NULL, NULL, 0)) < 0) {
+		rc = errno;
 		debug_perror("accept4() on TCP_REPAIR helper listening socket");
-		return;
+		return rc;
 	}
 
 	if (!getsockopt(c->fd_repair, SOL_SOCKET, SO_PEERCRED, &ucred, &len))
@@ -111,10 +115,14 @@ void repair_listen_handler(struct ctx *c, uint32_t events)
 	ev.events = EPOLLHUP | EPOLLET;
 	ev.data.u64 = ref.u64;
 	if (epoll_ctl(c->epollfd, EPOLL_CTL_ADD, c->fd_repair, &ev)) {
+		rc = errno;
 		debug_perror("epoll_ctl() on TCP_REPAIR helper socket");
 		close(c->fd_repair);
 		c->fd_repair = -1;
+		return rc;
 	}
+
+	return 0;
 }
 
 /**
@@ -145,29 +153,39 @@ void repair_handler(struct ctx *c, uint32_t events)
 /**
  * repair_wait() - Wait (with timeout) for TCP_REPAIR helper to connect
  * @c:		Execution context
+ *
+ * Return: 0 on success or if already connected, error code on failure
  */
-void repair_wait(struct ctx *c)
+int repair_wait(struct ctx *c)
 {
 	struct timeval tv = { .tv_sec = 0,
 			      .tv_usec = (long)(REPAIR_ACCEPT_TIMEOUT_US) };
+	int rc;
+
 	static_assert(REPAIR_ACCEPT_TIMEOUT_US < 1000 * 1000,
 		      ".tv_usec is greater than 1000 * 1000");
 
-	if (c->fd_repair >= 0 || c->fd_repair_listen == -1)
-		return;
+	if (c->fd_repair >= 0)
+		return 0;
+
+	if (c->fd_repair_listen == -1)
+		return ENOENT;
 
 	if (setsockopt(c->fd_repair_listen, SOL_SOCKET, SO_RCVTIMEO,
 		       &tv, sizeof(tv))) {
+		rc = errno;
 		err_perror("Set timeout on TCP_REPAIR listening socket");
-		return;
+		return rc;
 	}
 
-	repair_listen_handler(c, EPOLLIN);
+	rc = repair_listen_handler(c, EPOLLIN);
 
 	tv.tv_usec = 0;
 	if (setsockopt(c->fd_repair_listen, SOL_SOCKET, SO_RCVTIMEO,
 		       &tv, sizeof(tv)))
 		err_perror("Clear timeout on TCP_REPAIR listening socket");
+
+	return rc;
 }
 
 /**

repair.h
@@ -7,10 +7,10 @@
 #define REPAIR_H
 
 void repair_sock_init(const struct ctx *c);
-void repair_listen_handler(struct ctx *c, uint32_t events);
+int repair_listen_handler(struct ctx *c, uint32_t events);
 void repair_handler(struct ctx *c, uint32_t events);
 void repair_close(struct ctx *c);
-void repair_wait(struct ctx *c);
+int repair_wait(struct ctx *c);
 int repair_flush(struct ctx *c);
 int repair_set(struct ctx *c, int s, int cmd);
 