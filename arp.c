ea3dd28b546104c6bd4b4a4156d32d4f57b3d0cb
Laurent Vivier <lvivier@redhat.com>
2025-09-02 09:52:24 +0200

    arp: Don't mix incoming and outgoing buffers
    Don't use the memory of the incoming packet to build the outgoing buffer
    as it can be memory of the TX queue in the case of vhost-user.
    
    Moreover with vhost-user, the packet can be split across several
    iovec and it's easier to rebuild it in a buffer than updating an
    existing iovec array.
    
    Signed-off-by: Laurent Vivier <lvivier@redhat.com>
    Reviewed-by: David Gibson <david@gibson.dropbear.id.au>
    Signed-off-by: Stefano Brivio <sbrivio@redhat.com>

--- arp.c
+++ arp.c
@@ -31,6 +31,36 @@
 #include "tap.h"
 
 /**
+ * ignore_arp() - Check if we should ignore this ARP message
+ * @c:		Execution context
+ * @ah:		ARP header
+ * @am:		ARP message
+ *
+ * Return: true if the ARP message should be ignored, false otherwise
+ */
+static bool ignore_arp(const struct ctx *c,
+		       const struct arphdr *ah, const struct arpmsg *am)
+{
+	if (ah->ar_hrd != htons(ARPHRD_ETHER)	||
+	    ah->ar_pro != htons(ETH_P_IP)	||
+	    ah->ar_hln != ETH_ALEN		||
+	    ah->ar_pln != 4			||
+	    ah->ar_op  != htons(ARPOP_REQUEST))
+		return true;
+
+	/* Discard announcements, but not 0.0.0.0 "probes" */
+	if (memcmp(am->sip, &in4addr_any, sizeof(am->sip)) &&
+	    !memcmp(am->sip, am->tip, sizeof(am->sip)))
+		return true;
+
+	/* Don't resolve the guest's assigned address, either. */
+	if (!memcmp(am->tip, &c->ip4.addr, sizeof(am->tip)))
+		return true;
+
+	return false;
+}
+
+/**
  * arp() - Check if this is a supported ARP message, reply as needed
  * @c:		Execution context
  * @p:		Packet pool, single packet with Ethernet buffer
@@ -39,11 +69,14 @@
  */
 int arp(const struct ctx *c, const struct pool *p)
 {
-	unsigned char swap[4];
-	struct ethhdr *eh;
-	struct arphdr *ah;
-	struct arpmsg *am;
-	size_t l2len;
+	struct {
+		struct ethhdr eh;
+		struct arphdr ah;
+		struct arpmsg am;
+	} __attribute__((__packed__)) resp;
+	const struct ethhdr *eh;
+	const struct arphdr *ah;
+	const struct arpmsg *am;
 
 	eh = packet_get(p, 0, 0,			 sizeof(*eh), NULL);
 	ah = packet_get(p, 0, sizeof(*eh),		 sizeof(*ah), NULL);
@@ -52,35 +85,28 @@ int arp(const struct ctx *c, const struct pool *p)
 	if (!eh || !ah || !am)
 		return -1;
 
-	if (ah->ar_hrd != htons(ARPHRD_ETHER)	||
-	    ah->ar_pro != htons(ETH_P_IP)	||
-	    ah->ar_hln != ETH_ALEN		||
-	    ah->ar_pln != 4			||
-	    ah->ar_op  != htons(ARPOP_REQUEST))
+	if (ignore_arp(c, ah, am))
 		return 1;
 
-	/* Discard announcements, but not 0.0.0.0 "probes" */
-	if (memcmp(am->sip, &in4addr_any, sizeof(am->sip)) &&
-	    !memcmp(am->sip, am->tip, sizeof(am->sip)))
-		return 1;
+	/* Ethernet header */
+	resp.eh.h_proto = htons(ETH_P_ARP);
+	memcpy(resp.eh.h_dest, eh->h_source, sizeof(resp.eh.h_dest));
+	memcpy(resp.eh.h_source, c->our_tap_mac, sizeof(resp.eh.h_source));
 
-	/* Don't resolve the guest's assigned address, either. */
-	if (!memcmp(am->tip, &c->ip4.addr, sizeof(am->tip)))
-		return 1;
+	/* ARP header */
+	resp.ah.ar_op = htons(ARPOP_REPLY);
+	resp.ah.ar_hrd = ah->ar_hrd;
+	resp.ah.ar_pro = ah->ar_pro;
+	resp.ah.ar_hln = ah->ar_hln;
+	resp.ah.ar_pln = ah->ar_pln;
 
-	ah->ar_op = htons(ARPOP_REPLY);
-	memcpy(am->tha,		am->sha,	sizeof(am->tha));
-	memcpy(am->sha,		c->our_tap_mac,	sizeof(am->sha));
+	/* ARP message */
+	memcpy(resp.am.sha,		c->our_tap_mac,	sizeof(resp.am.sha));
+	memcpy(resp.am.sip,		am->tip,	sizeof(resp.am.sip));
+	memcpy(resp.am.tha,		am->sha,	sizeof(resp.am.tha));
+	memcpy(resp.am.tip,		am->sip,	sizeof(resp.am.tip));
 
-	memcpy(swap,		am->tip,	sizeof(am->tip));
-	memcpy(am->tip,		am->sip,	sizeof(am->tip));
-	memcpy(am->sip,		swap,		sizeof(am->sip));
-
-	l2len = sizeof(*eh) + sizeof(*ah) + sizeof(*am);
-	memcpy(eh->h_dest,	eh->h_source,	sizeof(eh->h_dest));
-	memcpy(eh->h_source,	c->our_tap_mac,	sizeof(eh->h_source));
-
-	tap_send_single(c, eh, l2len);
+	tap_send_single(c, &resp, sizeof(resp));
 
 	return 1;
 }
