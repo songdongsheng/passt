songdongsheng/passt

commit 251e676bd0bffe0c3b8bf0e925fbc87b6d4eeb6a

netlink: Return prefix length for IPv6 addresses in nl_addr_get()
nl_addr_get() was not setting the prefix_len output parameter for
IPv6 addresses, only for IPv4. This meant callers always got 0 for
IPv6, forcing them to use a hardcoded default (64).

Fix by assigning *prefix_len even in the IPv6 case.

Signed-off-by: Jon Maloy <jmaloy@redhat.com>
Reviewed-by: David Gibson <david@gibson.dropbear.id.au>
Signed-off-by: Stefano Brivio <sbrivio@redhat.com>

netlink.c
@@ -752,7 +752,7 @@ int nl_addr_set_ll_nodad(int s, unsigned int ifi)
  * @ifi:	Interface index in outer network namespace
  * @af:		Address family
  * @addr:	Global address to fill
- * @prefix_len:	Mask or prefix length, to fill (for IPv4)
+ * @prefix_len:	Mask or prefix length, to fill
  * @addr_l:	Link-scoped address to fill (for IPv6)
  *
  * Return: 0 on success, negative error code on failure
@@ -788,16 +788,11 @@ int nl_addr_get(int s, unsigned int ifi, sa_family_t af,
 			    (af == AF_INET6 && rta->rta_type != IFA_ADDRESS))
 				continue;
 
-			if (af == AF_INET && ifa->ifa_prefixlen > prefix_max) {
+			if (ifa->ifa_prefixlen > prefix_max &&
+			    (af == AF_INET || ifa->ifa_scope < RT_SCOPE_LINK)) {
 				memcpy(addr, RTA_DATA(rta), RTA_PAYLOAD(rta));
 
 				prefix_max = *prefix_len = ifa->ifa_prefixlen;
-			} else if (af == AF_INET6 && addr &&
-				   ifa->ifa_scope < RT_SCOPE_LINK &&
-				   ifa->ifa_prefixlen > prefix_max) {
-				memcpy(addr, RTA_DATA(rta), RTA_PAYLOAD(rta));
-
-				prefix_max = ifa->ifa_prefixlen;
 			}
 
 			if (addr_l &&

commit 831e98384b057a7e74aa615200752c1d84c017ee

netlink: Allow NULL to be passed as addr parameter to nl_addr_get (again)
A recent fix to nl_addr_get() unfortunately introduced a different problem.
251e676bd0bf removed a test if @addr was non-NULL.  conf_ip6() relied on
being able to pass NULL in order to update the link-local address without
updating the main address (because it was already set by the user).

This means that 'pasta -a 2001:db8::1' will SEGV.  Reverse the incorrect
part of the change to fix this.

Fixes: 251e676bd0bf ("netlink: Return prefix length for IPv6 addresses in nl_addr_get()")
Signed-off-by: David Gibson <david@gibson.dropbear.id.au>
Signed-off-by: Stefano Brivio <sbrivio@redhat.com>

netlink.c
@@ -788,7 +788,7 @@ int nl_addr_get(int s, unsigned int ifi, sa_family_t af,
 			    (af == AF_INET6 && rta->rta_type != IFA_ADDRESS))
 				continue;
 
-			if (ifa->ifa_prefixlen > prefix_max &&
+			if (ifa->ifa_prefixlen > prefix_max && addr &&
 			    (af == AF_INET || ifa->ifa_scope < RT_SCOPE_LINK)) {
 				memcpy(addr, RTA_DATA(rta), RTA_PAYLOAD(rta));
 