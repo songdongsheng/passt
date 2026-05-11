songdongsheng/passt

commit 4af3d83170fd227b177f9861d93391e3d2008d34

treewide: Fix more pointers which can be const
Here are some more pointers which can be const, as reported by cppcheck
2.19.1.

Signed-off-by: David Gibson <david@gibson.dropbear.id.au>
Signed-off-by: Stefano Brivio <sbrivio@redhat.com>

conf.c
@@ -1234,7 +1234,7 @@ dns6:
  *
  * Return: 0 on success, negative error code on failure
  */
-static int conf_runas(char *opt, unsigned int *uid, unsigned int *gid)
+static int conf_runas(const char *opt, unsigned int *uid, unsigned int *gid)
 {
 	const char *uopt, *gopt = NULL;
 	char *sep = strchr(opt, ':');

dhcpv6.c
@@ -320,7 +320,7 @@ static bool dhcpv6_opt(struct iov_tail *data, uint16_t type)
 static bool dhcpv6_ia_notonlink(struct iov_tail *data,
 				struct in6_addr *la)
 {
-	int ia_types[2] = { OPT_IA_NA, OPT_IA_TA }, *ia_type;
+	int ia_types[2] = { OPT_IA_NA, OPT_IA_TA };
 	struct opt_ia_addr opt_addr_storage;
 	const struct opt_ia_addr *opt_addr;
 	struct iov_tail current, ia_base;
@@ -330,6 +330,7 @@ static bool dhcpv6_ia_notonlink(struct iov_tail *data,
 	struct in6_addr req_addr;
 	struct opt_hdr h_storage;
 	const struct opt_hdr *h;
+	const int *ia_type;
 
 	foreach(ia_type, ia_types) {
 		current = *data;

vhost_user.c
@@ -442,7 +442,7 @@ static bool vu_set_mem_table_exec(struct vu_dev *vdev,
 	for (i = 0; i < vdev->memory.nregions; i++) {
 		struct vhost_user_memory_region *msg_region = &memory->regions[i];
 		struct vu_dev_region *dev_region = &vdev->memory.regions[i];
-		void *mmap_addr;
+		const void *mmap_addr;
 
 		debug("vhost-user region %d", i);
 		debug("    guest_phys_addr: 0x%016"PRIx64,

commit db02221ceee1ec01d74ed15d5d5e2d8997c54f51

conf: Fix not-actually-const parameter to conf_runas() and conf_ugid()
Commit 4af3d83170fd changed the @opt parameter to conf_runas() to a const
pointer, to remove a cppcheck error.  However, that error was a false
positive.  We *do* modify that parameter via an aliased pointer within it
retreived from strchr().  At least with gcc 16.1.1 that now causes a
"discards const" warning.  Revert that change and instead directly suppress
the cppcheck warning.

Fixes: 4af3d83170fd ("treewide: Fix more pointers which can be const")
Signed-off-by: David Gibson <david@gibson.dropbear.id.au>
Reviewed-by: Laurent Vivier <lvivier@redhat.com>
Signed-off-by: Stefano Brivio <sbrivio@redhat.com>

conf.c
@@ -925,7 +925,8 @@ dns6:
  *
  * Return: 0 on success, negative error code on failure
  */
-static int conf_runas(const char *opt, unsigned int *uid, unsigned int *gid)
+/* cppcheck-suppress [constParameterPointer,unmatchedSuppression] */
+static int conf_runas(char *opt, unsigned int *uid, unsigned int *gid)
 {
 	const char *uopt, *gopt = NULL;
 	char *sep = strchr(opt, ':');
@@ -977,7 +978,7 @@ static int conf_runas(const char *opt, unsigned int *uid, unsigned int *gid)
  * @uid:	User ID, set on success
  * @gid:	Group ID, set on success
  */
-static void conf_ugid(const char *runas, uid_t *uid, gid_t *gid)
+static void conf_ugid(char *runas, uid_t *uid, gid_t *gid)
 {
 	/* If user has specified --runas, that takes precedence... */
 	if (runas) {
@@ -1247,7 +1248,7 @@ void conf(struct ctx *c, int argc, char **argv)
 	uint8_t prefix_len_from_opt = 0;
 	unsigned int ifi4 = 0, ifi6 = 0;
 	const char *logfile = NULL;
-	const char *runas = NULL;
+	char *runas = NULL;
 	size_t logsize = 0;
 	long fd_tap_opt;
 	int name, ret;