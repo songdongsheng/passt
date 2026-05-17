songdongsheng/passt

commit e51494552b784861b58f9546a25dfd74b8c09de9

Fix build with -DNDEBUG
Since bc872d91765d, our assert() statements are omitted if we compile with
-DNDEBUG, like the standard library assert(3).  Unfortunately a trivial but
embarrassing mistake in that patch means that instead of never aborting in
this case, assert_with_msg() *always* aborts, breaking pretty much
everything.

There's also a missing #include that breaks the build with -DNDEBUG on at
least some library versions.

Reported-by: Jan Palus <jpalus@fastmail.com>
Fixes: bc872d91765d ("treewide: Spell ASSERT() as assert()")
Signed-off-by: David Gibson <david@gibson.dropbear.id.au>
[sbrivio: Explicitly avoid evaluation of expr in assert_with_msg on
 NDEBUG as suggested by Jan]
Signed-off-by: Stefano Brivio <sbrivio@redhat.com>

util.h
@@ -6,6 +6,7 @@
 #ifndef UTIL_H
 #define UTIL_H
 
+#include <assert.h>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <stdbool.h>
@@ -50,7 +51,7 @@ void abort_with_msg(const char *fmt, ...)
  */
 #ifndef NDEBUG
 #define assert_with_msg(expr, ...)					\
-	((expr) ? (void)0 : abort_with_msg(__VA_ARGS__))
+	(1 ? (void)0 : ((void)(expr), abort_with_msg(__VA_ARGS__)))
 /* The standard library assert() hits our seccomp filter and dies before it can
  * actually print a message.  So, replace it with our own version.
  */
@@ -60,7 +61,7 @@ void abort_with_msg(const char *fmt, ...)
 			__func__, __FILE__, __LINE__, STRINGIFY(expr))
 #else
 #define assert_with_msg(expr, ...)					\
-	((void)(expr), 0 ? (void)0 : abort_with_msg(__VA_ARGS__))
+	((void)(expr), 1 ? (void)0 : abort_with_msg(__VA_ARGS__))
 #endif
 
 #ifdef P_tmpdir

commit bcc3d37a6e010b9340d085482f98caab21833776

util: Fix changes to assert_with_msg()
A last minute fixup to commit e51494552b78 was misapplied.  Instead of
improving the implementation of assert_with_msg() for the case where
NDEBUG is defined, it effectively eliminated assert_with_msg() in the
normal case where NDEBUG is _not_ defined.  As well as removing assert()s
which protect us in case of certian types of bug, this introduces some
cppcheck and clang-tidy regressions.

Fixes: e51494552b78 ("Fix build with -DNDEBUG")
Signed-off-by: David Gibson <david@gibson.dropbear.id.au>
Signed-off-by: Stefano Brivio <sbrivio@redhat.com>

util.h
@@ -51,7 +51,7 @@ void abort_with_msg(const char *fmt, ...)
  */
 #ifndef NDEBUG
 #define assert_with_msg(expr, ...)					\
-	(1 ? (void)0 : ((void)(expr), abort_with_msg(__VA_ARGS__)))
+	((expr) ? (void)0 : abort_with_msg(__VA_ARGS__))
 /* The standard library assert() hits our seccomp filter and dies before it can
  * actually print a message.  So, replace it with our own version.
  */
@@ -61,7 +61,7 @@ void abort_with_msg(const char *fmt, ...)
 			__func__, __FILE__, __LINE__, STRINGIFY(expr))
 #else
 #define assert_with_msg(expr, ...)					\
-	((void)(expr), 1 ? (void)0 : abort_with_msg(__VA_ARGS__))
+	(1 ? (void)0 : ((void)(expr), abort_with_msg(__VA_ARGS__)))
 #endif
 
 #ifdef P_tmpdir