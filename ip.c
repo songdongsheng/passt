songdongsheng/passt

commit 93c3e351f23561331824f2a2305bde3628676db5

ip: Define a bound for the string returned by ipproto_name()
ipproto_name() returns a static string of theoretically unbounded length.
That's going to be inconvenient in future, so introduce IPPROTO_STRLEN
giving an explicit bound on the length.  Use static_assert() and some
macros to ensure nothing we return can exceed this.

Signed-off-by: David Gibson <david@gibson.dropbear.id.au>
Signed-off-by: Stefano Brivio <sbrivio@redhat.com>

ip.c
@@ -12,6 +12,7 @@
  * Author: Stefano Brivio <sbrivio@redhat.com>
  */
 
+#include <assert.h>
 #include <stddef.h>
 #include <netinet/in.h>
 
@@ -74,7 +75,7 @@ found:
  * ipproto_name() - Get IP protocol name from number
  * @proto:	IP protocol number
  *
- * Return: pointer to name of protocol @proto
+ * Return: pointer to name of protocol @proto (<= IPPROTO_STRLEN bytes)
  *
  * Usually this would be done with getprotobynumber(3) but that reads
  * /etc/protocols and might allocate, which isn't possible for us once
@@ -83,16 +84,21 @@ found:
 const char *ipproto_name(uint8_t proto)
 {
 	switch (proto) {
+#define CASE(s)								\
+		static_assert(sizeof(s) <= IPPROTO_STRLEN,		\
+			      "Increase IPPROTO_STRLEN to contain " #s); \
+		return s;
 	case IPPROTO_ICMP:
-		return "ICMP";
+		CASE("ICMP");
 	case IPPROTO_TCP:
-		return "TCP";
+		CASE("TCP");
 	case IPPROTO_UDP:
-		return "UDP";
+		CASE("UDP");
 	case IPPROTO_ICMPV6:
-		return "ICMPv6";
+		CASE("ICMPv6");
 	default:
-		return "<unknown protocol>";
+		CASE("<unknown protocol>");
+#undef CASE
 	}
 }
 

ip.h
@@ -118,6 +118,8 @@ static inline uint32_t ip6_get_flow_lbl(const struct ipv6hdr *ip6h)
 }
 
 bool ipv6_l4hdr(struct iov_tail *data, uint8_t *proto, size_t *dlen);
+
+#define IPPROTO_STRLEN		(sizeof("<unknown protocol>"))
 const char *ipproto_name(uint8_t proto);
 
 /* IPv6 link-local all-nodes multicast address, ff02::1 */

commit 196a9e555ef3507430948ee4cc46ad574d643965

ip: Wrap CASE macro body in braces for pre-C23 compatibility
Compiling on RHEL8 (gcc-8.5) gives an error in ip.c.
ip.c:88:3: error: a label can only be part of a statement and
a declaration is not a statement due to the use of static_assert.

The fix is to surround it with {}.

Link: https://bugs.passt.top/show_bug.cgi?id=201
Fixes: 93c3e351f235 ("ip: Define a bound for the string returned by ipproto_name()")
Signed-off-by: Anshu Kumari <anskuma@redhat.com>
Reviewed-by: David Gibson <david@gibson.dropbear.id.au>
Reviewed-by: Laurent Vivier <lvivier@redhat.com>
[sbrivio: Fixed coding style]
Signed-off-by: Stefano Brivio <sbrivio@redhat.com>

ip.c
@@ -34,10 +34,12 @@
 const char *ipproto_name(uint8_t proto)
 {
 	switch (proto) {
-#define CASE(s)								\
-		static_assert(sizeof(s) <= IPPROTO_STRLEN,		\
-			      "Increase IPPROTO_STRLEN to contain " #s); \
-		return s;
+#define CASE(s)								  \
+		{							  \
+			static_assert(sizeof(s) <= IPPROTO_STRLEN,	  \
+			       "Increase IPPROTO_STRLEN to contain " #s); \
+			return s;					  \
+		}
 	case IPPROTO_ICMP:
 		CASE("ICMP");
 	case IPPROTO_TCP: