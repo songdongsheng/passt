songdongsheng/passt

commit 4ddd59bc6085cbb87a3630e746323ac247164976

conf: Separate local mode for each IP version, don't enable disabled IP version
This fixes an issue and introduces a feature:

- in local mode, we accidentally ignored the -4 / --ipv4-only and
  -6 / --ipv6-only command line options

- if no template interface is available for a given IP version,
  instead of disabling that IP version, use local mode, separately,
  for it

Link: https://bugs.passt.top/show_bug.cgi?id=129
Link: https://bugs.passt.top/show_bug.cgi?id=128
Signed-off-by: Stefano Brivio <sbrivio@redhat.com>
Reviewed-by: David Gibson <david@gibson.dropbear.id.au>
Tested-by: Paul Holzinger <pholzing@redhat.com>

conf.c
@@ -1109,7 +1109,7 @@ static void conf_print(const struct ctx *c)
 		info("Template interface: %s%s%s%s%s",
 		     c->ifi4 > 0 ? if_indextoname(c->ifi4, ifn) : "",
 		     c->ifi4 > 0 ? " (IPv4)" : "",
-		     (c->ifi4 && c->ifi6) ? ", " : "",
+		     (c->ifi4 > 0 && c->ifi6 > 0) ? ", " : "",
 		     c->ifi6 > 0 ? if_indextoname(c->ifi6, ifn) : "",
 		     c->ifi6 > 0 ? " (IPv6)" : "");
 	}
@@ -2001,20 +2001,23 @@ void conf(struct ctx *c, int argc, char **argv)
 	    (*c->ip6.ifname_out && !c->ifi6))
 		die("External interface not usable");
 
+	if (!c->ifi4 && !c->ifi6 && !*c->pasta_ifn) {
+		strncpy(c->pasta_ifn, pasta_default_ifn,
+			sizeof(c->pasta_ifn) - 1);
+	}
 
-	if (!c->ifi4 && !c->ifi6) {
-		info("No external interface as template, switch to local mode");
+	if (!c->ifi4 && !v6_only) {
+		info("IPv4: no external interface as template, use local mode");
 
 		conf_ip4_local(&c->ip4);
 		c->ifi4 = -1;
+	}
+
+	if (!c->ifi6 && !v4_only) {
+		info("IPv6: no external interface as template, use local mode");
 
 		conf_ip6_local(&c->ip6);
 		c->ifi6 = -1;
-
-		if (!*c->pasta_ifn) {
-			strncpy(c->pasta_ifn, pasta_default_ifn,
-				sizeof(c->pasta_ifn) - 1);
-		}
 	}
 
 	if (c->ifi4 && !no_map_gw &&

passt.1
@@ -166,8 +166,8 @@ By default, assigned IPv4 and IPv6 addresses are taken from the host interfaces
 with the first default route, if any, for the corresponding IP version. If no
 default routes are available and there is any interface with any route for a
 given IP version, the first of these interfaces will be chosen instead. If no
-such interface exists, the link-local address 169.254.2.1 is assigned for IPv4,
-and no additional address will be assigned for IPv6.
+such interface exists for a given IP version, the link-local address 169.254.2.1
+is assigned for IPv4, and no additional address will be assigned for IPv6.
 
 .TP
 .BR \-n ", " \-\-netmask " " \fImask
@@ -194,9 +194,9 @@ first default route, if any, for the corresponding IP version. If the default
 route is a multipath one, the gateway is the first nexthop router returned by
 the kernel which has the highest weight in the set of paths. If no default
 routes are available and there is just one interface with any route, that
-interface will be chosen instead. If no such interface exists, the link-local
-address 169.254.2.2 is used for IPv4, and the link-local address fe80::1 is used
-for IPv6.
+interface will be chosen instead. If no such interface exists for a given IP
+version, the link-local address 169.254.2.2 is used for IPv4, and the link-local
+address fe80::1 is used for IPv6.
 
 Note: these addresses are also used as source address for packets directed to
 the guest or to the target namespace having a loopback or local source address,
@@ -1117,8 +1117,9 @@ throughput of TCP connections.
 .SS Local mode for disconnected setups
 
 If \fBpasst\fR and \fBpasta\fR fail to find a host interface with a configured
-address, other than loopback addresses, they will, obviously, not attempt to
-source addresses or routes from the host.
+address for a given IP version, other than loopback addresses, they will,
+obviously, not attempt to source addresses or routes from the host, for that
+IP version.
 
 In this case, unless configured otherwise, they will assign the IPv4 link-local
 address 169.254.2.1 to the guest or target namespace, and no IPv6 address. The

commit e1a6d9ef626aa6dbcfeef97dbbab3bd69c35b4b1

conf, util: Disable IPv6 if explicit IPv6 socket probe fails
In https://bugs.passt.top/show_bug.cgi?id=188, I originally reported
that if IPv6 is disabled in the kernel (for example via command line
parameter ipv6.disable=1, or disabled in build configuration), and we
attempt to forward any port, we'll exit right away after failing to
set up dual-stack listening sockets.

The original instance of that issue is now fixed for pasta by commit
75dcbc300bf0 ("pasta: Warn, disable matching IP version if not
supported, in local mode") together with the new implementation of
the rule forwarding table, starting from commit b223bec48213 ("fwd,
tcp, udp: Set up listening sockets based on forward table"), because
we first parse forwarding options, then probe for IPv6 support in the
target namespace (and disable IPv6 as a result), and finally bind
sockets once we already know that IPv6 support is disabled.

But we don't do that when invoked as passt, because we have no target
namespace and hence no probing for IPv6 support whatsoever.

Add IPv6 to the socket features we test in sock_probe_features(), and,
if we fail to create an IPv6 socket for whatever reason (which might
include security policies as well), disable IPv6 support altogether,
so that we won't attempt to use dual-stack sockets for port forwarding
either.

Note that the probe comes without any sort of debug message, because
at this point we haven't parsed the configuration yet, and we would
therefore print that regardless of the selected logging level and
other options, including --ipv4-only, which would be rather confusing.
I doubt we'll miss this kind of message though, IPv6 support being
disabled is anyway obvious from the initial configuration dump.

Reported-by: Chi Cuong HA <ChiCuong.HA@amadeus.com>
Reported-by: Romain Geissler <romain.geissler@amadeus.com>
Link: https://bugs.passt.top/show_bug.cgi?id=188
Fixes: 4ddd59bc6085 ("conf: Separate local mode for each IP version, don't enable disabled IP version")

conf.c
@@ -1254,7 +1254,9 @@ void conf(struct ctx *c, int argc, char **argv)
 	int name, ret;
 	uid_t uid;
 	gid_t gid;
-	
+
+	if (c->no_ipv6)
+		v4_only = true;
 
 	if (c->mode == MODE_PASTA)
 		c->no_dhcp_dns = c->no_dhcp_dns_search = 1;
@@ -1682,6 +1684,9 @@ void conf(struct ctx *c, int argc, char **argv)
 			v6_only = false;
 			break;
 		case '6':
+			if (c->no_ipv6)
+				die("IPv6 not available but --ipv6-only given");
+
 			v6_only = true;
 			v4_only = false;
 			break;

passt.h
@@ -211,6 +211,7 @@ struct ip6_ctx {
  * @low_wmem:		Low probed net.core.wmem_max
  * @low_rmem:		Low probed net.core.rmem_max
  * @no_bindtodevice:	Unprivileged SO_BINDTODEVICE not available
+ * @no_ipv6:		IPv6 sockets not available
  * @vdev:		vhost-user device
  * @device_state_fd:	Device state migration channel
  * @device_state_result: Device state migration result
@@ -296,6 +297,7 @@ struct ctx {
 	int low_wmem;
 	int low_rmem;
 	int no_bindtodevice;
+	bool no_ipv6;
 
 	struct vu_dev *vdev;
 

util.c
@@ -334,6 +334,14 @@ void sock_probe_features(struct ctx *c)
 		c->no_bindtodevice = 1;
 	}
 
+	/* Check if IPv6 sockets are usable */
+	close(s);
+	s = socket(AF_INET6, SOCK_STREAM | SOCK_CLOEXEC, IPPROTO_TCP);
+	if (s < 0) {
+		c->no_ipv6 = true;
+		return;
+	}
+
 	close(s);
 }
 