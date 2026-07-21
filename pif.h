songdongsheng/passt

commit cbcd4284111f56a233b0c68fba403650c1fb0ccc

pif: Limit pif names to 128 bytes
All current pif names are quite short, and we expect them to remain short
when/if we allow arbitrary pifs.  However, because of the structure of
the current code we don't enforce any limit on the length.

This will become more important with dynamic configuration updates, so
start enforcing a length limit.  Specifically we allow pif names to be up
to 128 bytes (PIF_NAME_SIZE), including the terminating \0.  This is
more or less arbitrary, but seems like it should be comfortably enough for
all the cases we have in mind.

Signed-off-by: David Gibson <david@gibson.dropbear.id.au>
Reviewed-by: Laurent Vivier <lvivier@redhat.com>
[sbrivio: Fixed typo in comment, reported by Laurent]
Signed-off-by: Stefano Brivio <sbrivio@redhat.com>

pif.h
@@ -35,7 +35,9 @@ enum pif_type {
 	PIF_NUM_TYPES,
 };
 
-extern const char *pif_type_str[];
+/* Maximum size of a pif name, including \0 */
+#define	PIF_NAME_SIZE	(128)
+extern const char pif_type_str[][PIF_NAME_SIZE];
 
 static inline const char *pif_type(enum pif_type pt)
 {
@@ -43,6 +45,7 @@ static inline const char *pif_type(enum pif_type pt)
 		return pif_type_str[pt];
 	else
 		return "?";
+	static_assert(sizeof("?") <= PIF_NAME_SIZE);
 }
 
 static inline const char *pif_name(uint8_t pif)

pif.c
@@ -17,7 +17,7 @@
 #include "inany.h"
 #include "epoll_ctl.h"
 
-const char *pif_type_str[] = {
+const char pif_type_str[][PIF_NAME_SIZE] = {
 	[PIF_NONE]		= "<none>",
 	[PIF_HOST]		= "HOST",
 	[PIF_TAP]		= "TAP",

commit 316cb2e74751ca1bffbb265cd8531126f5a74aa2

pif: Add message to static_assert for C11 compliance, fix build with gcc 8
Seen with gcc 8.5:

---
In file included from passt.h:18,
                 from qrap.c:36:
pif.h: In function 'pif_type':
pif.h:48:44: error: expected ',' before ')' token
  static_assert(sizeof("?") <= PIF_NAME_SIZE);
                                            ^
make: *** [Makefile:105: qrap] Error 1
---

As noted in ba84a3b17af8 ("treewide: Add messages to static_assert()
calls"), static_assert() calls need a message to comply with C11. The
form without message is supported starting from C23 only.

Fixes: cbcd4284111f ("pif: Limit pif names to 128 bytes")
Signed-off-by: Stefano Brivio <sbrivio@redhat.com>
Reviewed-by: David Gibson <david@gibson.dropbear.id.au>

pif.h
@@ -45,7 +45,7 @@ static inline const char *pif_type(enum pif_type pt)
 		return pif_type_str[pt];
 	else
 		return "?";
-	static_assert(sizeof("?") <= PIF_NAME_SIZE);
+	static_assert(sizeof("?") <= PIF_NAME_SIZE, "PIF_NAME_SIZE too small");
 }
 
 static inline const char *pif_name(uint8_t pif)